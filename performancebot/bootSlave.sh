75571e6402a7caf8c68f60b29b2f9b1f7f6d0006
Bernhard Urban <bernhard.urban@xamarin.com>
2015-07-20 13:06:04 -0700

    pbot: using mono ubuntu repository to get a recent system mono on the slave

--- performancebot/bootSlave.sh
+++ performancebot/bootSlave.sh
@@ -14,7 +14,10 @@ for i in "/usr/bin/dpkg" "/bin/cp" "/bin/rm"; do
     sudo -n "$i" --help &> /dev/null || (echo "/etc/sudoers must have the following line:" && echo "`whoami` `hostname` = (root) NOPASSWD: $i" && exit 1)
 done
 
-echo "TODO: http://www.mono-project.com/docs/getting-started/install/linux/#debian-ubuntu-and-derivatives"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
 
 sudo apt-get install python-pip python-dev mono-runtime
 sudo pip install virtualenv
