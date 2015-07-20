0b4c555a37dfdb4e01ecc0a2ac5a7557cf55b5e0
Vlad Brezae <brezaevlad@gmail.com>
2015-07-20 15:13:25 -0700

    Do not hardcode the parse credentials.
    Fetch them securely from the parse service, which uses github OAuth. Cache the credentials on the disk so we don't need to log in at every run.

---
+++ tools/common/Models/Credentials.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Benchmarker.Common.Models
+{
+	public class Credentials
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+
+		public Credentials ()
+		{
+		}
+
+		public static void WriteToFile (Credentials credentials, string filename)
+		{
+			try {
+				using (FileStream fileStream = File.Open (filename, FileMode.Create, FileAccess.Write)) {
+					using (var streamWriter = new StreamWriter (fileStream)) {
+						streamWriter.Write (JsonConvert.SerializeObject (credentials));
+					}
+				}
+			} catch (Exception) {
+				Console.WriteLine ("Failed to save credentials to disk");
+			}
+		}
+
+		public static Credentials LoadFromFile (string filename)
+		{
+			try {
+				using (var reader = new StreamReader (new FileStream (filename, FileMode.Open))) {
+					return Credentials.LoadFromString (reader.ReadToEnd ());
+				}
+			} catch (Exception) {
+				return null;
+			}
+		}
+
+		public static Credentials LoadFromString (string str)
+		{
+			try {
+				return JsonConvert.DeserializeObject<Credentials> (str);
+			} catch (Exception) {
+				return null;
+			}
+		}
+	}
+}
