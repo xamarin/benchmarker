b10f4a2df836b53798270726cc2110662240fee7
Mark Probst <mark.probst@gmail.com>
2015-08-20 15:54:27 -0700

    Fetch pull request title from GitHub in front-end page.

--- front-end/src/pullrequest.js
+++ front-end/src/pullrequest.js
@@ -7,6 +7,7 @@ import * as xp_utils from './utils.js';
 import * as xp_charts from './charts.js';
 import {Parse} from 'parse';
 import React from 'react';
+import GitHub from 'github-api';
 
 class Controller extends xp_common.Controller {
 
@@ -61,23 +62,41 @@ class Controller extends xp_common.Controller {
 }
 
 type PullRequestDescriptionProps = {
-	pullRequest: Parse.Object | void;
+	pullRequest: Parse.Object;
 };
 
 class PullRequestDescription extends React.Component<PullRequestDescriptionProps, PullRequestDescriptionProps, void> {
+    constructor (props) {
+        super (props);
+        this.state = { title: undefined };
+
+        var pr = this.props.pullRequest;
+        var match = pr.get ('URL').match (/^https?:\/\/github\.com\/mono\/mono\/pull\/(\d+)\/?$/);
+        if (match !== null) {
+            var github = new GitHub ({});
+            var repo = github.getRepo ("mono", "mono");
+            repo.getPull (match [1], (err, info) => {
+                console.log ("got pull: ", info);
+                if (info.title !== undefined)
+                    this.setState ({ title: info.title });
+            });
+        }
+    }
+
 	render () : Object {
 		var pr = this.props.pullRequest;
-
-		if (pr === undefined)
-			return <div></div>;
 
         var baselineRunSet = pr.get ('baselineRunSet');
         var baselineHash = baselineRunSet.get ('commit').get ('hash');
 
+        var title = "Link";
+        if (this.state.title !== undefined)
+            title = this.state.title;
+
 		return <div className="Description">
 			<dl>
 			<dt>Pull Request</dt>
-            <dd><a href={pr.get ('URL')}>Link</a></dd>
+            <dd><a href={pr.get ('URL')}>{title}</a></dd>
 			<dt>Baseline commit</dt>
 			<dd><a href={xp_common.githubCommitLink (baselineHash)}>{baselineHash.substring (0, 10)}</a></dd>
 			</dl>
