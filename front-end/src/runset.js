41f4a161af072efe84cf4f339b37c5b4c0c8ebcb
Jon Purdy <evincarofautumn@gmail.com>
2015-08-27 15:25:47 -0700

    Improve design of run set data table.

--- front-end/src/runset.js
+++ front-end/src/runset.js
@@ -122,7 +122,8 @@ class RunSetDescription extends React.Component {
 		var buildURL = runSet.get ('buildURL');
 		var buildLink;
 		var logURLs = runSet.get ('logURLs');
-		var logLinks;
+		var logLinks = [];
+		var logLinkList;
 		var timedOutBenchmarks;
 		var crashedBenchmarks;
 		var table;
@@ -133,39 +134,53 @@ class RunSetDescription extends React.Component {
 		if (logURLs !== undefined && Object.keys (logURLs).length !== 0) {
 			for (var key in logURLs) {
 				var url = logURLs [key];
-				logLinks.push(<li>{key}: <a href={url}>{url}</a></li>);
+				var anchor = document.createElement ('a');
+				anchor.href = url;
+				
+				var shortUrl = <span>{anchor.hostname}/&hellip;{anchor.pathname.substring (anchor.pathname.lastIndexOf ('/'))}</span>;
+				logLinks.push(<li><a href={url}><code>{key}</code> ({shortUrl})</a></li>);
+			}
+			if (logLinks.length === 0) {
+				logLinkList = undefined;
+			} else {
+				logLinkList = <ul>{logLinks}</ul>;
 			}
 		}
-
-		var timedOutString = xp_common.joinBenchmarkNames (this.props.controller, runSet.get ('timedOutBenchmarks'), "");
-		if (timedOutString !== "")
-			timedOutBenchmarks = timedOutString;
-
-		var crashedString = xp_common.joinBenchmarkNames (this.props.controller, runSet.get ('crashedBenchmarks'), "");
-		if (crashedString !== "")
-			crashedBenchmarks = crashedString;
 
 		if (this.state.runs === undefined) {
 			table = <div className='DiagnosticBlock'>Loading run data&hellip;</div>;
 		} else {
 			var runsByBenchmarkName = xp_utils.partitionArrayByString (this.state.runs, r => this.props.controller.benchmarkNameForId (r.get ('benchmark').id));
+			var crashedBenchmarkIds = runSet.get ('crashedBenchmarks').map (b => b.id);
+			var timedOutBenchmarkIds = runSet.get ('timedOutBenchmarks').map (b => b.id);
 			var benchmarkNames = Object.keys (runsByBenchmarkName);
 			benchmarkNames.sort ();
 			table = <table>
 				<tr>
 					<th>Benchmark</th>
+					<th>Status</th>
 					<th>Elapsed Times (ms)</th>
 					<th>Bias due to Outliers</th>
 				</tr>
 				{benchmarkNames.map (name => {
 					var runs = runsByBenchmarkName [name];
-					var benchmark = this.props.controller.benchmarkForId (runs [0].get ('benchmark').id);
+					var benchmarkId = runs [0].get ('benchmark').id;
+					var benchmark = this.props.controller.benchmarkForId (benchmarkId);
 					var elapsed = runs.map (r => r.get ('elapsedMilliseconds'));
 					elapsed.sort ();
 					var elapsedString = elapsed.join (", ");
 					var outlierVariance = xp_common.outlierVariance (elapsed);
+					var statusIcons = [];
+					if (crashedBenchmarkIds.indexOf (benchmarkId) !== -1)
+						statusIcons.push (<span className="statusIcon crashed fa fa-exclamation-circle" title="Crashed"></span>);
+					if (timedOutBenchmarkIds.indexOf (benchmarkId) !== -1)
+						statusIcons.push (<span className="statusIcon timedOut fa fa-clock-o" title="Timed Out"></span>);
+					if (statusIcons.length === 0)
+						statusIcons.push (<span className="statusIcon good fa fa-check" title="Good"></span>);
+
 					return <tr className={benchmark.get ('disabled') ? 'disabled' : ''}>
 						<td><code>{name}</code>{benchmark.get ('disabled') ? ' (disabled)' : ''}</td>
+						<td className="statusColumn">{statusIcons}</td>
 						<td>{elapsedString}</td>
 						<td>
 							<div className="degree" title={outlierVariance}>
@@ -180,13 +195,9 @@ class RunSetDescription extends React.Component {
 		var commitHash = runSet.get ('commit').get ('hash');
 		var commitLink = xp_common.githubCommitLink (commitHash);
 
-		var openCompare = function () { window.open('compare.html#' + runSet.id); };
 		return <div className="Description">
-			<h1><a href={commitLink}>{commitHash.substring (0, 10)}</a> ({buildLink})</h1>
-			<p><button onClick={openCompare}>Compare</button></p>
-			<ul>{logLinks}</ul>
-			<p>Timed out: {timedOutBenchmarks || "none"}</p>
-			<p>Crashed: {crashedBenchmarks || "none"}</p>
+			<h1><a href={commitLink}>{commitHash.substring (0, 10)}</a> ({buildLink}, <a href={'compare.html#' + runSet.id}>compare</a>)</h1>
+			{logLinkList}
 			{table}
 		</div>;
 	}
