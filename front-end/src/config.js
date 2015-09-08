25ce9c5b47be0a31fa4f15e3ad8fe6a35091f7f5
Mark Probst <mark.probst@gmail.com>
2015-09-08 16:01:22 -0700

    Only fetch necessary data in config and machine pages.

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -3,16 +3,28 @@
 "use strict";
 
 import * as xp_common from './common.js';
-import * as xp_utils from './utils.js';
+import {Parse} from 'parse';
 import React from 'react';
 
 class Controller extends xp_common.Controller {
-
 	configId: string | void;
+	config: Parse.Object | void;
 
 	constructor (configId) {
 		super ();
 		this.configId = configId;
+	}
+
+	loadAsync () {
+		var query = new Parse.Query (xp_common.Config);
+		query.get (this.configId, {
+			success: obj => {
+				this.config = obj;
+				this.allDataLoaded ();
+			},
+			error: error => {
+				alert ("error loading config: " + error.toString ());
+			}});
 	}
 
 	allDataLoaded () {
@@ -21,15 +33,12 @@ class Controller extends xp_common.Controller {
 				<xp_common.Navigation currentPage="" />
 				<article>
 					<xp_common.ConfigDescription
-						config={xp_utils.find (
-							this.allConfigs,
-							config => config.id === this.configId)} />
+						config={this.config} />
 				</article>
 			</div>,
 			document.getElementById ('configPage')
 		);
 	}
-
 }
 
 function started () {
--- front-end/src/machine.js
+++ front-end/src/machine.js
@@ -3,16 +3,28 @@
 "use strict";
 
 import * as xp_common from './common.js';
-import * as xp_utils from './utils.js';
+import {Parse} from 'parse';
 import React from 'react';
 
 class Controller extends xp_common.Controller {
-
 	machineId: string | void;
+	machine: Parse.Object | void;
 
 	constructor (machineId) {
 		super ();
 		this.machineId = machineId;
+	}
+
+	loadAsync () {
+		var query = new Parse.Query (xp_common.Machine);
+		query.get (this.machineId, {
+			success: obj => {
+				this.machine = obj;
+				this.allDataLoaded ();
+			},
+			error: error => {
+				alert ("error loading machine: " + error.toString ());
+			}});
 	}
 
 	allDataLoaded () {
@@ -21,9 +33,7 @@ class Controller extends xp_common.Controller {
 				<xp_common.Navigation currentPage="" />
 				<article>
 					<xp_common.MachineDescription
-						machine={xp_utils.find (
-							this.allMachines,
-							machine => machine.id === this.machineId)} />
+						machine={this.machine} />
 				</article>
 			</div>,
 			document.getElementById ('machinePage')
