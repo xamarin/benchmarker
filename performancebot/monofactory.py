6a8448e8b5aa5216f37eeca2a1bb8138d581fc15
Bernhard Urban <bernhard.urban@xamarin.com>
2015-09-15 11:50:06 -0700

    pbot: be more specific what to build

--- performancebot/monofactory.py
+++ performancebot/monofactory.py
@@ -63,7 +63,7 @@ class DebianMonoBuildFactory(BuildFactory):
                     'cd %s && ' % MASTERWORKDIR +
                     'git clone --depth 1 -b master https://github.com/xamarin/benchmarker && ' +
                     'cd benchmarker/tools && (nuget restore tools.sln || nuget restore tools.sln) ' + #nuget crashes sometimes :-(
-                    '&& xbuild && ' +
+                    '&& xbuild /t:compare && ' +
                     'cd ../.. && tar cvfz benchmarker.tar.gz benchmarker/tools/{*.dll,*.exe} && (md5 benchmarker.tar.gz || md5sum benchmarker.tar.gz)'
                 )
                 ]
