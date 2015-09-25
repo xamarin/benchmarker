19bdf5ed946593d9c0edb960f61091aced44b828
Bernhard Urban <bernhard.urban@xamarin.com>
2015-09-25 16:53:25 -0700

    tools: split common into libdbmodel, liblogging, libgithub and libpostgres. rename accredit to libaccredit

---
+++ tools/libgithub/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace libgithub
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ tools/libpostgres/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace libpostgres
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
