18ea6b450da840e31054f4e5e13e92e8a38c1ae9
Bernhard Urban <bernhard.urban@xamarin.com>
2015-09-28 16:03:10 -0700

    pbot: get pkg from wrench build site

--- performancebot/wrenchpoller.py
+++ performancebot/wrenchpoller.py
@@ -9,6 +9,7 @@ from buildbot.process.buildstep import BuildStep, LoggingBuildStep
 from buildbot.changes import base
 from buildbot.util import epoch2datetime
 
+from HTMLParser import HTMLParser
 import operator
 import json
 from constants import MONO_BASEURL, MONO_PULLREQUEST_BASEURL, MONO_COMMON_SNAPSHOTS_URL, MONO_SOURCETARBALL_URL, MONO_SOURCETARBALL_PULLREQUEST_URL, PROPERTYNAME_JENKINSBUILDURL, PROPERTYNAME_JENKINSGITCOMMIT, PROPERTYNAME_JENKINSGITHUBPULLREQUEST, FORCE_PROPERTYNAME_JENKINS_BUILD, Lane
@@ -19,19 +20,23 @@ import credentials
 # https://wrench.internalx.com/Wrench/index.aspx?lane=mono-master-monodroid
 WRENCH_INTERNAL_BASE_URL = r'https://wrench.internalx.com'
 
-def _mk_request_wrench(base_url, lane, logger):
+def _mk_request_wrench_build_matrix(base_url, lane, logger):
     url = (base_url + '/Wrench/index.aspx?lane=' + lane).encode('ascii', 'ignore')
     if logger:
         logger("request: " + str(url))
     params = credentials.getWrenchHeader()
     return getPage(url, headers=params)
 
-# accept 'success' and 'issues' as class
+def _mk_request_wrench_single_build(url, logger):
+    url = url.encode('ascii', 'ignore')
+    if logger:
+        logger("request: " + str(url))
+    params = credentials.getWrenchHeader()
+    return getPage(url, headers=params)
 
-from HTMLParser import HTMLParser
+# accept 'success' and 'issues'
 class HTMLParserWrenchBuildMatrix(HTMLParser):
     def __init__(self, *args, **kwargs):
-        self.common_deb = None
         self.state = 0
         self.successful_builds = {}
         HTMLParser.__init__(self, *args, **kwargs)
@@ -71,6 +76,39 @@ class HTMLParserWrenchBuildMatrix(HTMLParser):
             self.successful_builds[data] = self.nextUrl
             self.state = 1
 
+class HTMLParserWrenchSingleBuild(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        self.state = 0
+        self.dmgpkg = None
+        HTMLParser.__init__(self, *args, **kwargs)
+
+    def handle_starttag(self, tag, attrs):
+        if self.state == 0 and tag == 'a':
+            self.state = 1
+        elif self.state == 2 and tag == 'td':
+            for name, value in attrs:
+                if name != 'class':
+                    continue
+                if value == 'success':
+                    self.state = 3
+        elif self.state == 3 and tag == 'a':
+            for name, value in attrs:
+                if name != 'href':
+                    continue
+                self.potentialLink = value
+
+    def handle_endtag(self, tag):
+        if self.state == 1 and tag == 'a':
+            self.state = 0
+
+    def handle_data(self, data):
+        if self.state == 1 and data == 'upload-to-storage':
+            self.state = 2
+        elif self.state == 3:
+            if data.startswith('mono-android-') and data.endswith('.pkg'):
+                print "HIT: " + str(data) + ", with: " + self.potentialLink
+                self.dmgpkg = self.potentialLink
+
 
 # for testing/debugging
 if __name__ == '__main__':
@@ -81,20 +119,29 @@ if __name__ == '__main__':
             print "stopping..."
         #pylint: enable=E1101
 
+    def _logger(msg):
+        print msg
+
     @defer.inlineCallbacks
     def test_mono_master_monodroid():
-        def _logger(msg):
-            print msg
-
         parserbuildmatrix = HTMLParserWrenchBuildMatrix()
-        parserbuildmatrix.feed((yield _mk_request_wrench(WRENCH_INTERNAL_BASE_URL, 'mono-master-monodroid', _logger)))
+        parserbuildmatrix.feed((yield _mk_request_wrench_build_matrix(WRENCH_INTERNAL_BASE_URL, 'mono-master-monodroid', _logger)))
         sorted_list = sorted(parserbuildmatrix.successful_builds.items(), key=operator.itemgetter(1))
         for revision, url in sorted_list:
             print "{0}: {1}".format(revision, WRENCH_INTERNAL_BASE_URL + "/Wrench/" + url)
 
     @defer.inlineCallbacks
+    def test_single_build():
+        url = r'https://wrench.internalx.com/Wrench/ViewLane.aspx?lane_id=1845&host_id=163&revision_id=588019'
+        parsersinglebuild = HTMLParserWrenchSingleBuild()
+        page = yield _mk_request_wrench_single_build(url, _logger)
+        print page
+        parsersinglebuild.feed(page)
+
+    @defer.inlineCallbacks
     def run_tests():
-        _ = yield test_mono_master_monodroid()
+        # _ = yield test_mono_master_monodroid()
+        _ = yield test_single_build()
         stop_me()
 
     run_tests()
