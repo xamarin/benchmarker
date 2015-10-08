660353a486c3fe176b3c4df15d8731f4dbf59f86
Bernhard Urban <bernhard.urban@xamarin.com>
2015-10-08 15:02:57 -0700

    xtcloghelper: check if a certain job ID from DB queue has finished

--- tools/xtcloghelper/Program.cs
+++ tools/xtcloghelper/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Benchmarker;
-using Benchmarker.Models;
+using bm = Benchmarker.Models;
 using System;
 using System.Text.RegularExpressions;
 using Common.Logging.Simple;
 using Common.Logging;
 using Npgsql;
 using System.Collections.Generic;
+using Xamarin.TestCloud.Api.V0;
+using Nito.AsyncEx;
 
 namespace xtclog
 {
@@ -35,9 +37,20 @@ namespace xtclog
 				var connection = PostgresInterface.Connect ();
 				PushXTCJobId (connection, xtcJobId);
 			} else if (args [0] == "--crawl-logs") {
-				Console.WriteLine ("NIY");
+				var connection = PostgresInterface.Connect ();
+				var xtcapikey = Accredit.GetCredentials ("xtcapikey") ["xtcapikey"].ToString ();
+				var xtcapi = new Client (xtcapikey);
+
+				foreach (var xtcjobid in PullXTCJobIds (connection)) {
+					Console.WriteLine ("XTC Job ID Pending: " + xtcjobid);
+					var guid = Guid.Parse (xtcjobid);
+					Console.WriteLine ("guid: \"{0}\"", guid);
+					ResultCollection results = AsyncContext.Run (() => xtcapi.TestRuns.Results (guid));
+					Console.WriteLine ("finished? " + results.Finished);
+				}
 				string text = System.IO.File.ReadAllText("/Users/bernhardu/work/benchmarker/tools/device-log-test.log");
-				ProcessLog (text);
+				var runSet = ProcessLog (text);
+				// TODO: runSet.UploadToPostgres (dbConnection, machine);
 			} else {
 				UsageAndExit (false);
 			}
@@ -49,24 +62,32 @@ namespace xtclog
 			PostgresInterface.Insert<long> (conn, "XamarinTestcloudJobIDs", row, "id");
 		}
 
-		private static void ProcessLog(string log) {
+		private static List<string> PullXTCJobIds(NpgsqlConnection conn) {
+			var l = new List<string> ();
+			foreach (var s in PostgresInterface.Select (conn, "XamarinTestcloudJobIDs", new string[] {"job"}, null, null)) {
+				l.Add (s.GetReference<string> ("job"));
+			}
+			return l;
+		}
+
+		private static bm.RunSet ProcessLog(string log) {
 			string logURL = null; // TODO
 			string regex_commit = @"I\/benchmarker\(\s*\d+\): Benchmarker \| commit ""(?<hash>[0-9A-Za-z]{40})"" on branch ""(?<branch>[\w\-_\.]+)""";
 			Match match_commit = Regex.Match (log, regex_commit);
-			var commit = new Commit ();
+			var commit = new bm.Commit ();
 			commit.Hash = match_commit.Groups ["hash"].Value;
 			commit.Branch = match_commit.Groups ["branch"].Value;
 
 			string regex_machine = @"I\/benchmarker\(\s*\d+\): Benchmarker \| hostname ""(?<hostname>[\w\s\.]+)"" architecture ""(?<architecture>[\w\-]+)""";
 			Match match_machine = Regex.Match (log, regex_machine);
-			var machine = new Machine {
+			var machine = new bm.Machine {
 				Name = match_machine.Groups ["hostname"].Value,
 				Architecture = match_machine.Groups ["architecture"].Value
 			};
 
 			string regex_config = @"I\/benchmarker\(\s*\d+\): Benchmarker \| configname ""(?<name>[\w\-\.]+)""";
 			Match match_config = Regex.Match (log, regex_config);
-			var config = new Config {
+			var config = new bm.Config {
 				Name = match_config.Groups ["name"].Value,
 				Mono = String.Empty,
 				MonoOptions = new string[0],
@@ -74,7 +95,7 @@ namespace xtclog
 				Count = 10
 			};
 
-			var runSet = new RunSet {
+			var runSet = new bm.RunSet {
 				StartDateTime = DateTime.Now, // TODO: get more precise data from log
 				Config = config,
 				Commit = commit,
@@ -96,25 +117,23 @@ namespace xtclog
 			}
 
 			foreach (string benchmark in bench_results.Keys) {
-				var result = new Result {
+				var result = new bm.Result {
 					DateTime = DateTime.Now,
-					Benchmark = new Benchmark { Name = benchmark},
+					Benchmark = new bm.Benchmark { Name = benchmark},
 					Config = config
 				};
 
 				foreach (TimeSpan t in bench_results[benchmark]) {
-					var run = new Result.Run ();
-					run.RunMetrics.Add (new Result.RunMetric {
-						Metric = Result.RunMetric.MetricType.Time,
+					var run = new bm.Result.Run ();
+					run.RunMetrics.Add (new bm.Result.RunMetric {
+						Metric = bm.Result.RunMetric.MetricType.Time,
 						Value = t
 					});
 					result.Runs.Add (run);
 				}
 				runSet.Results.Add (result);
 			}
-
-			// TODO: runSet.UploadToPostgres (dbConnection, machine);
-			Console.WriteLine ("hum: " + runSet.ToString ());
+			return runSet;
 		}
 	}
 }
