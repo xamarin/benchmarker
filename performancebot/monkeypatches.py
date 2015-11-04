992ca49bb56a71e03abd8fc11f4845029a14f8f9
Bernhard Urban <bernhard.urban@xamarin.com>
2015-11-03 16:37:52 -0800

    pbot: monkeypatch JSON encoder for LogFile class

--- performancebot/monkeypatches.py
+++ performancebot/monkeypatches.py
@@ -1,10 +1,21 @@
+# define __repr__ for LogFile class
 from buildbot.status.logfile import LogFile
 
 def logfile_to_string(self):
     return "<logfile=" + self.getFilename() + ">"
 
 
+# special handle LogFile in json encoder
+import json
+
+def json_encoder_default(self, o):
+    if isinstance(o, LogFile):
+        return str(o)
+    else:
+        raise TypeError(repr(o) + " is not JSON serializable")
 
 
+# entry point
 def apply_all_monkeypatches():
     LogFile.__repr__ = logfile_to_string
+    json.JSONEncoder.default = json_encoder_default
