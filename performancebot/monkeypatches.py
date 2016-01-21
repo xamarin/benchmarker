b4304c5b3fc99fa3783c5c88a8fe25b151c7fa9b
Bernhard Urban <bernhard.urban@xamarin.com>
2016-01-21 08:23:23 -0800

    pbot: extend expiration for oauth cookie

--- performancebot/monkeypatches.py
+++ performancebot/monkeypatches.py
@@ -1,10 +1,11 @@
+###########################################################
 # define __repr__ for LogFile class
 from buildbot.status.logfile import LogFile
 
 def logfile_to_string(self):
     return "<logfile=" + self.getFilename() + ">"
 
-
+###########################################################
 # special handle LogFile in json encoder
 import json
 
@@ -14,8 +15,19 @@ def json_encoder_default(_, o):
     else:
         raise TypeError(repr(o) + " is not JSON serializable")
 
+###########################################################
+# longer expiration
+from buildbot.status.web.session import Session
+from datetime import datetime, timedelta
 
+def renew_longer(self):
+    # expires in 60 days
+    self.expiration = datetime.now() + timedelta(60)
+    return self.expiration
+
+###########################################################
 # entry point
 def apply_all_monkeypatches():
     LogFile.__repr__ = logfile_to_string
     json.JSONEncoder.default = json_encoder_default
+    Session.renew = renew_longer
