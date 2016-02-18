e46591ed6403c27c5cb99c0169856f288ee3995b
Mark Probst <mark.probst@gmail.com>
2016-02-18 12:43:05 -0800

    Docker fixes.

--- performancebot/docker/build-slave.sh
+++ performancebot/docker/build-slave.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -f Dockerfile.master -t pbot-slave .
+docker build -f Dockerfile.ec2slave -t pbot-slave .
 
 docker tag -f pbot-slave 633007691302.dkr.ecr.us-east-1.amazonaws.com/pbot-slave:latest
 docker push 633007691302.dkr.ecr.us-east-1.amazonaws.com/pbot-slave:latest
