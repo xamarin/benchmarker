2b165708babbf07482d3e145251c16fac4444c19
Bernhard Urban <bernhard.urban@xamarin.com>
2016-03-15 13:41:31 -0700

    pbot: guard upload gc pause times with a timeout

--- performancebot/monosteps.py
+++ performancebot/monosteps.py
@@ -149,7 +149,7 @@ class ProcessBinaryProtocolFiles(MasterShellCommand):
         else:
             masterworkdir = 'tmp/' + str(self.getProperty('buildername')) + '/' + str(self.getProperty('buildnumber'))
             compare_cmd = lambda logfile, runid: 'mono ' + masterworkdir + '/benchmarker/tools/compare.exe --upload-pause-times ' + logfile + ' --sgen-grep-binprot ' + MONO_SGEN_GREP_BINPROT_FILENAME + ' --run-id ' + str(runid) + ' || failed=1; rm -rf ' + logfile + '; '
-            self.command = ['bash', '-x', '-c', 'failed=0; ' + "".join([compare_cmd(log_full_path, runid) for (log_full_path, runid) in zip(logs_full_path, runids)]) + ' if [ "$failed" == "1" ]; then exit 1; fi ']
+            self.command = ['timeout', '--foreground', '--signal=9', '360', 'bash', '-x', '-c', 'failed=0; ' + "".join([compare_cmd(log_full_path, runid) for (log_full_path, runid) in zip(logs_full_path, runids)]) + ' if [ "$failed" == "1" ]; then exit 1; fi ']
         MasterShellCommand.start(self)
 
 
